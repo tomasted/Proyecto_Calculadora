396cfb4483dbb6aa32f4995565ccecb44459840c
Tomas Escudero <tomas@gmail.com>
2024-04-19 09:06:42 +0200

    proyecto base master

---
+++ src/negocio/CalculadoraDao.java
@@ -0,0 +1,11 @@
+package negocio;
+
+public interface CalculadoraDao {
+	
+	int sumar(int i, int j);
+	int restar(int i, int j);
+	int multiplicar(int i, int j);
+	int dividir(int i, int j);
+	
+
+}
